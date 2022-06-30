4a4a1700f44dfea33d7d42e1791dadfcf0d3d7c1
hreddy <harishreddy616@gmail.com>
2022-06-30 14:28:40 +0530

    Change to message

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Harry!')
+        'body': json.dumps('Hello Harry! Works... ?')
     }
